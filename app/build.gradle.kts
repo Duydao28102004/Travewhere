903daeeb1f942e8bcda01d5c206bf89c6de5acc7
Duydao28102004 <duybaodao28102005@gmail.com>
2024-12-30 13:14:19 +0700

    add main activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.auth)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
