90830882bc55b0a5bc4d8478c22c7441bb2345a6
tungtran <tranluuquangtung159@gmail.com>
2024-12-30 19:09:25 +0700

    Update Login Layout

--- app/src/main/java/com/example/travewhere/AuthenticationActivity.java
+++ app/src/main/java/com/example/travewhere/AuthenticationActivity.java
@@ -1,24 +1,39 @@
 package com.example.travewhere;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.LinearLayout;
 
 public class AuthenticationActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private LinearLayout loginLayout, signupLayout;
+    private TextView switchToSignup, switchToLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_authentication);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        loginLayout = findViewById(R.id.loginLayout);
+        signupLayout = findViewById(R.id.signupLayout);
+        loginButton = findViewById(R.id.loginButton);
+
+        switchToLogin = findViewById(R.id.switchToLogin);
+        switchToSignup = findViewById(R.id.switchToSignup);
+
+        switchToLogin.setOnClickListener(v -> {
+            loginLayout.setVisibility(View.VISIBLE);
+            signupLayout.setVisibility(View.GONE);
+        });
+
+        switchToSignup.setOnClickListener(v -> {
+            loginLayout.setVisibility(View.GONE);
+            signupLayout.setVisibility(View.VISIBLE);
         });
     }
 }
