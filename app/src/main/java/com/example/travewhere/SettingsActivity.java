a8a4b4dc6d9c7a255cc1021b353c7d20183d846a
Minh <s3979367@rmit.edu.vn>
2025-01-01 22:28:55 +0700

    initialized and set up OnClickListener wireframe for all the buttons/tabs

--- app/src/main/java/com/example/travewhere/SettingsActivity.java
+++ app/src/main/java/com/example/travewhere/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.example.travewhere;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +17,45 @@ public class SettingsActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_settings);
 
+        // Initialize the settings buttons and set their onClickListeners
+        LinearLayout languageSettingsButtonButton = findViewById(R.id.language_settings);
+        languageSettingsButtonButton.setOnClickListener(v -> {
+            // Open the language settings activity
+        });
 
+        LinearLayout preferencesSettingsButton = findViewById(R.id.preference_settings);
+        preferencesSettingsButton.setOnClickListener(v -> {
+            // Open the preferences settings activity
+        });
+
+        LinearLayout securitySettingsButton = findViewById(R.id.security_settings);
+        securitySettingsButton.setOnClickListener(v -> {
+            // Open the security settings activity
+        });
+
+        LinearLayout currencySettingsButton = findViewById(R.id.currency_settings);
+        currencySettingsButton.setOnClickListener(v -> {
+            // Open the currency settings activity
+        });
+
+        LinearLayout aboutSettingsButton = findViewById(R.id.about_us_settings);
+        aboutSettingsButton.setOnClickListener(v -> {
+            // Open the about settings activity
+        });
+
+        LinearLayout faqSettingsButton = findViewById(R.id.faq_settings);
+        faqSettingsButton.setOnClickListener(v -> {
+            // Open the faq settings activity
+        });
+
+        LinearLayout contactUsSettingsButton = findViewById(R.id.contact_us_settings);
+        contactUsSettingsButton.setOnClickListener(v -> {
+            // Open the contact us settings activity
+        });
+
+        LinearLayout customLogoutButton = findViewById(R.id.logout_button);
+        customLogoutButton.setOnClickListener(v -> {
+            // Logout the user
+        });
     }
 }
