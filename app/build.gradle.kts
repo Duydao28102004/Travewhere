1e1376e94de6ba05352bd3da1a89c652cc05225f
Minh <s3979367@rmit.edu.vn>
2025-01-02 17:30:50 +0700

    created PreferencesSettingsActivity UI with theme switching toggle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ android {
 
 dependencies {
 
+    implementation(libs.appcompat.v161)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.firebase.firestore)
