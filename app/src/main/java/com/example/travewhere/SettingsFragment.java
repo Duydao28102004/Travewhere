0d8d58baf787edadeb5bdf90a7f3480ae8d493c7
Minh <s3979367@rmit.edu.vn>
2025-01-04 18:01:19 +0700

    initialised SettingsFragment

--- app/src/main/java/com/example/travewhere/SettingsFragment.java
+++ app/src/main/java/com/example/travewhere/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.example.travewhere;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +8,61 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class SettingsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        // Initialize the settings buttons and set their onClickListeners
+        LinearLayout languageSettingsButtonButton = view.findViewById(R.id.language_settings);
+        languageSettingsButtonButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), LanguageSettingsActivity.class);
+            startActivity(intent);
+        });
+
+        LinearLayout preferencesSettingsButton = view.findViewById(R.id.preferences_settings);
+        preferencesSettingsButton.setOnClickListener(v -> {
+            // Open the preferences settings activity
+            Intent intent = new Intent(getActivity(), PreferencesSettingsActivity.class);
+            startActivity(intent);
+        });
+
+        LinearLayout securitySettingsButton = view.findViewById(R.id.security_settings);
+        securitySettingsButton.setOnClickListener(v -> {
+            // Open the security settings activity
+        });
+
+        LinearLayout currencySettingsButton = view.findViewById(R.id.currency_settings);
+        currencySettingsButton.setOnClickListener(v -> {
+            // Open the currency settings activity
+        });
+
+        LinearLayout aboutSettingsButton = view.findViewById(R.id.about_us_settings);
+        aboutSettingsButton.setOnClickListener(v -> {
+            // Open the about settings activity
+        });
+
+        LinearLayout faqSettingsButton = view.findViewById(R.id.faq_settings);
+        faqSettingsButton.setOnClickListener(v -> {
+            // Open the faq settings activity
+        });
+
+        LinearLayout contactUsSettingsButton = view.findViewById(R.id.contact_us_settings);
+        contactUsSettingsButton.setOnClickListener(v -> {
+            // Open the contact us settings activity
+        });
+
+        LinearLayout customLogoutButton = view.findViewById(R.id.logout_button);
+        customLogoutButton.setOnClickListener(v -> {
+            // Logout the user
+        });
+
+        return view;
     }
 }
