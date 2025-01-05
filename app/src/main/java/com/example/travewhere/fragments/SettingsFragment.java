be54c027b4e9b3973d1eff375a049cd30c879924
Minh <s3979367@rmit.edu.vn>
2025-01-05 21:12:20 +0700

    re-implemented UI and extracted string resources for SettingsFragment

--- app/src/main/java/com/example/travewhere/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/travewhere/fragments/SettingsFragment.java
@@ -1,4 +1,4 @@
-package com.example.travewhere;
+package com.example.travewhere.fragments;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.travewhere.R;
 
 /**
  * A simple {@link Fragment} subclass.
