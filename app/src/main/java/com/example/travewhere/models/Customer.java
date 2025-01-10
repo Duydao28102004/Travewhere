4c24560efb6c20d982fea11458c7efca208204c7
Minh <s3979367@rmit.edu.vn>
2025-01-10 09:04:15 +0700

    added MemberStatus model to store the data for displaying member status into LoyaltyActivity; implement the getMemberStatus() in Customer model to update the respected member status data

--- app/src/main/java/com/example/travewhere/models/Customer.java
+++ app/src/main/java/com/example/travewhere/models/Customer.java
@@ -35,11 +35,11 @@ public class Customer extends User implements Serializable {
     // Return the member status based on the point
     public MemberStatus getMemberStatus() {
         if (point >= 1000) {
-            return new MemberStatus("Diamond Member", R.drawable.ic_diamond_user);
+            return new MemberStatus("Diamond Member", R.drawable.ic_diamond_user, "As a Diamond Member, you will receive exclusive 10% discount on all bookings domestically", "1 Special gift + 1 3D-2N trip to Saigon on your birthday month", "When travelling to Europe/America, you will receive 3 free days of trip");
         } else if (point >= 500) {
-            return new MemberStatus("Gold Member", R.drawable.ic_gold_user);
+            return new MemberStatus("Gold Member", R.drawable.ic_gold_user, "As a Gold Member, you will receive exclusive 10% discount on 5 bookings domestically", "1 Special gift on your birthday month", "When travelling to Vietnam's provinces, you will receive 3 free days of trip");
         } else {
-            return new MemberStatus("Bronze Member", R.drawable.ic_bronze_user);
+            return new MemberStatus("Bronze Member", R.drawable.ic_bronze_user, "As a Bronze Member, you will receive exclusive 5% discount on 1 booking domestically", "1 Free day of trip to RMIT Saigon South Campus", "No special offer available");
         }
     }
 }
--- app/src/main/java/com/example/travewhere/models/MemberStatus.java
+++ app/src/main/java/com/example/travewhere/models/MemberStatus.java
@@ -3,10 +3,16 @@ package com.example.travewhere.models;
 public class MemberStatus {
     private final String title;
     private final int iconResId;
+    private final String exclusiveDiscountDescription;
+    private final String exclusiveBirthdayGiftDescription;
+    private final String exclusiveSpecialOfferDescription;
 
-    public MemberStatus(String title, int iconResId) {
+    public MemberStatus(String title, int iconResId, String exclusiveDiscountDescription, String exclusiveBirthdayGiftDescription, String exclusiveSpecialOfferDescription) {
         this.title = title;
         this.iconResId = iconResId;
+        this.exclusiveDiscountDescription = exclusiveDiscountDescription;
+        this.exclusiveBirthdayGiftDescription = exclusiveBirthdayGiftDescription;
+        this.exclusiveSpecialOfferDescription = exclusiveSpecialOfferDescription;
     }
 
     public String getTitle() {
@@ -15,5 +21,17 @@ public class MemberStatus {
 
     public int getIconResId() {
         return iconResId;
+    }
+
+    public String getExclusiveDiscountDescription() {
+        return exclusiveDiscountDescription;
+    }
+
+    public String getExclusiveBirthdayGiftDescription() {
+        return exclusiveBirthdayGiftDescription;
+    }
+
+    public String getExclusiveSpecialOfferDescription() {
+        return exclusiveSpecialOfferDescription;
     }
 }
